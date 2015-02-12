105cd23bd213d867f1adb0caa9964d87c945eee6
Julia Edholm <dramagirl1012@yahoo.com>
2015-02-12 09:17:08 -0500

    Trying to create graph of vertices
    Created Tile Class, Vertex Class, GraphMaker Class

--- Game/src/GamePlayer.java
+++ Game/src/GamePlayer.java
@@ -10,28 +10,36 @@ public class GamePlayer {
 	
 	Player[] players;
 	
+	
 	public GamePlayer(int numPlayers){
-		//create stats table for players
-		//create Player classes for each player and store in players[]
-		int[][] board= new Board().getBoard();
-		createGraph(board);
-		Visualizer GUI = new Visualizer(board);
-		//add event listeners to gUI and figure out how GamePlayer will capture the click events 
+		
 	}
 	
-	
-	//Go through the board and create verticies where appropriate
-	private static void createGraph(int[][] board){
-		//board[0][i]= the resource type of tile i
-		//board[1][i] = the probability of tile i
+	public static void main (String[] args){
 		
-		//adjacentTiles[0] = the resource type of tile x, adjacentTiles[1]= roll #
+		//create stats table for players
+		//create Player classes for each player and store in players[]
+	//	int[][] board= new Board().getBoard();
 		
-		Vertex top = new Vertex({board[0][0],board[1][0]});
-		for (int i= 0; i < board.length; i++){
-			//looking at first tile
-			
-		}
+	/*	int[][] testBoard =  {
+				{2,6},
+				  {3,8},
+				  {4, 11},
+				  {5,2},
+				  {1,5},
+				  {3,6},
+				  {1,11},
+		};
+		*/
+		int[][] testBoard = {
+				{3,2,5,1,4,2,4},//resource
+				{6,8,11,2,5,6,11},//roll 
+		};
+		
+		new GraphMaker(testBoard);
+		//Visualizer GUI = new Visualizer(board);
+		//add event listeners to gUI and figure out how GamePlayer will capture the click events 
+
 	}
 
 	
---
+++ Game/src/Tile.java
@@ -0,0 +1,28 @@
+/*JE
+ * This class refers to a specific tile on the board and contains information about the tile's:
+ *  resource type 
+ *  roll
+ *  robber status
+ */
+public class Tile {
+	int resource;
+	int roll;
+	boolean hasRobber = false;
+	
+	public Tile(int resourceNum, int rollNum){
+		resource = resourceNum;
+		roll = rollNum;
+	}
+	
+	public int getResource(){
+		return resource;
+	}
+	
+	public void printTile(){
+		System.out.println(Resource.getType(resource)+"("+roll+")");
+	}
+	
+	public String getString(){
+		return Resource.getType(resource)+"("+roll+")";
+	}
+}
