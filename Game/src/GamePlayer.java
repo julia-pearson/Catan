2ded27b78bfe3d6a6ff20cbd12ba6762093fce62
cjbernstein <carolynjenniferbernstein@gmail.com>
2015-02-13 13:48:14 -0500

    updates to game player event handling shell

--- Game/src/GamePlayer.java
+++ Game/src/GamePlayer.java
@@ -1,4 +1,6 @@
-/*JE */
+//JE +CJ
+
+import java.util.*;
 
 public class GamePlayer {
 	/*	-Tasks:
@@ -7,44 +9,74 @@ public class GamePlayer {
 	3. control user input
 	4. update tables/board when user gives input 
 	*/
-	
-	Player[] players;
-	
-	
-	public GamePlayer(int numPlayers){
-		
-	}
-	
+
+	private static Scanner sc = new Scanner(System.in);
+	private Player[] players;
+
+	//RUN WITH NUMBER OF PLAYERS AS COMMAND LINE ARGUMENT
 	public static void main (String[] args){
-		
-		//create stats table for players
-		//create Player classes for each player and store in players[]
-	//	int[][] board= new Board().getBoard();
-		
+
+		//creates Player classes for each player and stores in players[]
+		int numPlayers = sc.nextInt();
+		players = new Player[3];
+		for(int i=1; i<(numPlayers+1); i++)
+			players[i] = new Player(i);
+
+		//create board
+		//testboard gives a predetermined board
 		int[][] testBoard = new Board().getTestBoard();
+		//int[][] board= new Board().getBoard();
 		
-		new GraphMaker(testBoard);
+		//create graph and GUI from the board
+		//new GraphMaker(testBoard);
 		//Visualizer GUI = new Visualizer(board);
+
+
 		//add event listeners to gUI and figure out how GamePlayer will capture the click events 
 
+		//play da game
+
 	}
 	
-	public void buildSettlement(Player p, int VertexNumber){
-		//check that the player has resource to build
-		//check that it is a legal move
+	public void buildSettlement(Player p, int vertexNumber){
+		//CJ: check that the player has resources to build a settlement and has settlements left
+		//Julia E: check that it is a legal move on the graph (ie roads built to there, no one else build there, 2 away)
+		//CJ: update resource cards and victory points
+		//Julia P: update graphics
 	}
 
-	public void buildCity(Player p, int VertexNumber){
-		//check that the player has resource to build
-		//check that it is a legal move
+	public void buildCity(Player p, int vertexNumber){
+		//CJ: check that the player has resources to build a city and has cities left
+		//Julia E: check that it is a legal move on the graph (ie settlement already there)
+		//CJ: update resource cards and victory points
+		//Julia P: update graphics
 	}
 	
 	public void buildRoad(Player p, int roadNum){
-		//check that the player has resource to build
-		//check that it is a legal move
+		//CJ: check that the player has resources to build a road and has roads left
+		//Julia E: check that it is a legal move on the graph (ie no other road there, road connected. if first road, make sure one of each settlement)
+		//CJ: update resource cards and longest road stuff
+		//Julia P: update graphics
+	}
+
+	public void buildDevCard(Player p){
+		//CJ: check that the player has resources to build a d card. update shit.
+		//Julia P: update graphics
+	}
+
+	public void useDevCard(Player p){
+		//CJ: ask which one and see if the player has that dev card
+		//Julia P: update graphics
+	}
+
+	public void usePort(Player p){
+		//CJ: check that the player has the port and update stats
+		//Julia P: update graphics
+	}
+
+	public void trade(Player p){
+		//meep
 	}
 	
-	private static void playGame(){
-		//method that will enable users to roll dice and interact
-	}
+	
 }
