b757335f7c9133a649a4da00db63003689822070
julia-pearson <julia.pearson@gmail.com>
2015-03-10 23:54:56 -0400

    stats

--- Game/src/Hexanew.java
+++ Game/src/Hexanew.java
@@ -244,10 +244,10 @@ public class Hexanew extends JFrame{
     {4,4},
   };
 
-  int[] player1 = new int[]{0,0,0,0,0,0};
-  int[] player2 = new int[]{0,0,0,0,0,0};
-  int[] player3 = new int[]{0,0,0,0,0,0};
-  int[] player4 = new int[]{0,0,0,0,0,0};
+  int[] player1 = new int[]{0,0,0,0,0,0,0,0,0,0};
+  int[] player2 = new int[]{0,0,0,0,0,0,0,0,0,0};
+  int[] player3 = new int[]{0,0,0,0,0,0,0,0,0,0};
+  int[] player4 = new int[]{0,0,0,0,0,0,0,0,0,0};
 
 
   //sets up screen and size
@@ -529,7 +529,20 @@ public class Hexanew extends JFrame{
   }
 
   public void addStatistics(int[] statistics, int player){
-    
+    for(int i=5;i<11;i++){
+      if (currentPlayer==1) {
+        player1[i]=statistics[i];        
+      }
+      if (currentPlayer==2) {
+        player2[i]=statistics[i];        
+      }
+      if (currentPlayer==3) {
+        player3[i]=statistics[i];        
+      }
+      if (currentPlayer==4) {
+        player4[i]=statistics[i];        
+      }
+    }
   }
 
 
@@ -869,26 +882,34 @@ public class Hexanew extends JFrame{
       repaint(); 
   }
 
-  public void drawStatistics(Graphics2D g2){
-    /*
+  public void drawStatistics(Graphics2D g2, int currentPlayer){
       Font font = new Font("Gill Sans", Font.PLAIN, 15);
       g2.setFont(font);
       //draws cards
       g2.setColor(stone);
 
-      String vp = Integer.toString(resourceCount[1]);
-      if(stone.equals("0")){
-        stone=" ";
+      if(currentPlayer==1){
+        String vp = Integer.toString(player1[5]);        
       }
-      String wheat = Integer.toString(resourceCount[2]);
+      if(currentPlayer==2){
+        String vp = Integer.toString(player2[5]);        
+      }
+      if(currentPlayer==3){
+        String vp = Integer.toString(player3[5]);        
+      }
+      if(currentPlayer==4){
+        String vp = Integer.toString(player4[5]);        
+      }
+
+/*      String dv = Integer.toString(resourceCount[2]);
       if(wheat.equals("0")){
         wheat=" ";
       }
-
+*/
       int i=currentPlayer-1;
-      g2.drawString("Victory Points   "+, x+250, y+25+i*150);
-      g2.drawString("Development Cards    "+, x+250, y+40+i*150); 
-   */ 
+      g2.drawString("Victory Points   "+dv, x+250, y+25+i*150);
+      g2.drawString("Development Cards    ", x+250, y+40+i*150); 
+   
   }
 
   public void tradeButton(int x, int y){
